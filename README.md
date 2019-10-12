f05b98c1e71c7cdf93a9268af828907a535ac863
Mitchel Sellers <msellers@Iowacomputergurus.com>
2019-10-12 11:06:59 -0400

    Initial Web & Test Project Setup

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# UnitTestingPatternsDemo
-A demonstration project for discussing Unit Testing Patterns with .NET Core and XUnit
+# Unit Testing Patterns Demo
+
+This is a companion project to go along with a presentation on Effective Unit Testing with .NET Core.
+
+## Sample Projects
+
+Included in this project are the following projects.
+
+*  Sample.Web - A Sample MVC Project
+*  Sample.Web.Tests - A Sample MVC Project Test Project
+*  Sample.Services - A sample Services project
+*  Sample.Services.Tests - A sample test project for services
---
+++ source/Sample.Web.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Sample.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Sample.Web.Tests.Controllers
+{
+    public class HomeControllerTests
+    {
+        private readonly HomeController _controller;
+        private readonly Mock<ILogger<HomeController>> _loggerHomeControllerMock;
+
+        public HomeControllerTests()
+        {
+            _loggerHomeControllerMock = new Mock<ILogger<HomeController>>();
+            _controller = new HomeController(_loggerHomeControllerMock.Object);
+        }
+
+        [Fact]
+        public void Privacy_ReturnsAViewResult_WithNullModel()
+        {
+            //Arrange
+
+            //Act
+            var result = _controller.Privacy();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+            Assert.Null(viewResult.Model);
+        }
+
+        [Fact]
+        public void Index_ReturnsAViewResult_WithNullModel()
+        {
+            //Arrange
+
+            //Act
+            var result = _controller.Index();
+
+            //Assert
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+        }
+    }
+}
